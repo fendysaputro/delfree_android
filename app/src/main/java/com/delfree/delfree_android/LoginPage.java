b4bf630673b933dc45083af5e33272532d986f6f
Fendy Saputro <fendy@amg.co.id>
2019-08-06 15:01:46 +0700

    update login

--- app/src/main/java/com/delfree/delfree_android/LoginPage.java
+++ app/src/main/java/com/delfree/delfree_android/LoginPage.java
@@ -1,7 +1,14 @@
 package com.delfree.delfree_android;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LoginPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage);
+    }
 
 }
