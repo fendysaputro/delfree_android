be0a8aef3f20a266714416a3feb67bf4640d0d07
Fendy Saputro <fendy@amg.co.id>
2019-08-09 09:20:54 +0700

    update login and add bottomNavigation

--- app/src/main/java/com/delfree/delfree_android/MainActivity.java
+++ app/src/main/java/com/delfree/delfree_android/MainActivity.java
@@ -1,7 +1,12 @@
 package com.delfree.delfree_android;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by phephen on 6/8/19.
@@ -9,9 +14,35 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    Toast.makeText(getApplicationContext(), "this is HOme", Toast.LENGTH_LONG).show();
+                    return true;
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.title_history);
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
