1f5f4305b381c84b1c92bbed69747717a1b0c0e6
Fendy Saputro <fendy@amg.co.id>
2019-08-16 19:51:34 +0700

    update

--- app/src/main/java/com/delfree/delfree_android/DbHelper.java
+++ app/src/main/java/com/delfree/delfree_android/DbHelper.java
@@ -7,14 +7,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.delfree.delfree_android.Model.Tracking;
+
 public class DbHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "com_batavree.db";
     public static final String GPSTRACKING_TABLE_NAME = "tracking";
-    public static final String DRAFT_COLUMN_ID = "id";
-    public static final String DRAFT_COLUMN_DATE = "date";
-    public static final String DRAFT_COLUMN_LOCATION_LAT = "location_lat";
-    public static final String DRAFT_COLUMN_LOCATION_LONG = "location_long";
+    public static final String TRACKING_COLUMN_ID = "id";
+    public static final String TRACKING_COLUMN_DATE = "date";
+    public static final String TRACKING_COLUMN_LOCATION_LAT = "location_lat";
+    public static final String TRACKING_COLUMN_LOCATION_LONG = "location_long";
 
 
     public DbHelper(Context context) {
@@ -36,9 +38,9 @@ public class DbHelper extends SQLiteOpenHelper {
     public boolean insertTracking(String date, double location_lat, double location_long) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(DRAFT_COLUMN_DATE, date);
-        contentValues.put(DRAFT_COLUMN_LOCATION_LAT, location_lat);
-        contentValues.put(DRAFT_COLUMN_LOCATION_LONG, location_long);
+        contentValues.put(TRACKING_COLUMN_DATE, date);
+        contentValues.put(TRACKING_COLUMN_LOCATION_LAT, location_lat);
+        contentValues.put(TRACKING_COLUMN_LOCATION_LONG, location_long);
         Log.i("AMG", contentValues.toString());
         db.insert(GPSTRACKING_TABLE_NAME, null, contentValues);
         return true;
@@ -49,5 +51,15 @@ public class DbHelper extends SQLiteOpenHelper {
         return db.delete(GPSTRACKING_TABLE_NAME,
                 "id = ? ",
                 new String[] { Integer.toString(id) });
+    }
+
+    public Tracking getTracking(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from"+GPSTRACKING_TABLE_NAME+" where id="+id+"", null);
+        Tracking retTracking = new Tracking(res.getInt(res.getColumnIndex(TRACKING_COLUMN_ID)),
+                res.getString(res.getColumnIndex(TRACKING_COLUMN_DATE)),
+                res.getDouble(res.getColumnIndex(TRACKING_COLUMN_LOCATION_LAT)),
+                res.getDouble(res.getColumnIndex(TRACKING_COLUMN_LOCATION_LONG)));
+        return retTracking;
     }
 }
